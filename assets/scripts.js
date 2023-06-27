b4561932f29167138fb2453070ce06f968d94bae
Domitius <133902698+recenasu@users.noreply.github.com>
2023-06-27 15:03:57 -0700

    more sandbox stuff

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,19 +1,21 @@
 console.log("hello");
 console.log("My name is Slim");
 
-var pirate1Name = "Blackbeard"
-var pirate2Name = "Jean Lafitte"
-var pirate3Name = "Edward Teach"
-var pirate4Name = "Jack Sparrow"
-var numPiratesCarribean = 4
-var numPiratesPacific = 5
-var numPiratesAtlantic = 8
-var numPiratesIndian = 2
-var numPiratesArctic = 2
-var areSeasSafe = false
-var tooManyPirates = true
+var pirate1Name = "Blackbeard";
+var pirate2Name = "Jean Lafitte";
+var pirate3Name = "Edward Teach";
+var pirate4Name = "Jack Sparrow";
+var numPiratesCarribean = 4;
+var numPiratesPacific = 5;
+var numPiratesAtlantic = 8;
+var numPiratesIndian = 2;
+var numPiratesArctic = 2;
+var areSeasSafe = false;
+var tooManyPirates = true;
+var numPiratesTotal = numPiratesArctic + numPiratesAtlantic + numPiratesCarribean + numPiratesIndian + numPiratesPacific;
+var maxNumPirates = 20;
 
-pirate2Name = "Dominique You"
+pirate2Name = "Dominique You";
 
 console.log(pirate1Name + " and " + pirate2Name);
 console.log(pirate2Name + " had a pirate base near New Orleans");
@@ -31,3 +33,20 @@ console.log(numPiratesArctic <= numPiratesAtlantic);
 console.log(numPiratesCarribean == numPiratesAtlantic);
 console.log(tooManyPirates && areSeasSafe);
 console.log(tooManyPirates || areSeasSafe);
+console.log(numPiratesTotal);
+
+if (numPiratesTotal >= maxNumPirates) {
+    console.log("Dead men tell no tales!");
+    tooManyPirates = true;
+} else {
+    console.log("Ahoy, matey!")
+    tooManyPirates = false;
+    areSeasSafe = true;
+}
+
+if (areSeasSafe) {
+    console.log("Set sail!");
+} else {
+    console.log("Shiver me timbers!");
+}
+
